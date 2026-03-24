b38c0de773256d3c6434fa1d8562d314a129e3c9
Raihan Sukmana <raihansukmana12@gmail.com>
2026-03-25 01:42:30 +1000

    Fix cloudflare pages issue

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { copyFileSync } from 'fs'
 
-export default defineConfig({ plugins: [react()], base: '/' })
+export default defineConfig({
+  plugins: [
+    react(),
+    {
+      name: 'copy-index-to-404',
+      closeBundle() {
+        copyFileSync('dist/index.html', 'dist/404.html')
+      }
+    }
+  ],
+  base: '/'
+})
